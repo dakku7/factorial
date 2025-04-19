4bbfd313c76ed0e60acdbe720c56a3b7deef5fdf
dakku <116499879+dakku7@users.noreply.github.com>
2025-04-20 00:33:59 +0200

    Add files via upload

---
+++ факториал!/факториал!.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int factorial(int n) {
+
+	if (n == 1) {
+		return 1;
+	}
+
+	if (n == 0) {
+		std::cout << "Zero cannot be!\n";
+		return 0;
+	}
+	std::cout << n << " * "; // Выводим текущий множитель
+
+	return n * factorial(n - 1);
+}
+
+
+int main() {
+
+	std::cout << "Its a factorial program!\n";
+
+	int n;
+
+	std::cout << "Enter a number to calculate its factorial: "; std::cin >> n;
+
+	std::cout << "You entered: " << n << "!" << std::endl;
+
+	std:: cout <<  "1 = " << factorial(n);
+
+	return 0;
+}
